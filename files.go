ac5345d14799cef155d2945b0f4b690e59e54ae4
Tyr Mactire <tyr@pettingzoo.co>
2022-04-30 00:19:34 -0700

    name files

--- files.go
+++ files.go
@@ -4,6 +4,8 @@ import "embed"
 
 // Files contains static files required by the application
 //go:embed locales/active.*.toml
-//go:embed web/static/*
+//go:embed web/static/css/default.min.css
+//go:embed web/static/css/error.min.css
+//go:embed web/static/css/login.min.css
 //go:embed web/template/*
 var Files embed.FS
